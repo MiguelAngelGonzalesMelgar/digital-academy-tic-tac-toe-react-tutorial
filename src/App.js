899565a509843e96ec559e1a10e5f5ee0f42154d
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-05-22 15:07:29 -0400

    refactor: lift state up to board. Add handler to render square value from top level state.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import { useState } from "react"
 
 // Square component
-const Square = () => {
-  // State
-  const [value, setValue] = useState(null);
+const Square = ({ value, onSquareClick }) => {
 
-  // Handler
-  const handleClick = () => {
-    setValue("X");
-  }
-  return <button className="square" onClick={handleClick}>{ value }</button>
+  return <button className="square"onClick={onSquareClick}>{ value }</button>
 }
 
 // Main board
 const Board = () => {
+  // Top state
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  // Handler
+  const handleSquareClick = (i) => {
+    const nextSquares = [...squares];
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[0]} onSquareClick={() => handleSquareClick(0)}/>
+      <Square value={squares[1]} onSquareClick={() => handleSquareClick(1)}/>
+      <Square value={squares[2]} onSquareClick={() => handleSquareClick(2)}/>
     </div>
 
     <div className="board-row">
-    <Square />
-    <Square />
-    <Square />
+    <Square value={squares[3]} onSquareClick={() => handleSquareClick(3)}/>
+    <Square value={squares[4]} onSquareClick={() => handleSquareClick(4)}/>
+    <Square value={squares[5]} onSquareClick={() => handleSquareClick(5)}/>
     </div>
 
     <div className="board-row">
-    <Square />
-    <Square />
-    <Square />
+    <Square value={squares[6]} onSquareClick={() => handleSquareClick(6)}/>
+    <Square value={squares[7]} onSquareClick={() => handleSquareClick(7)}/>
+    <Square value={squares[8]} onSquareClick={() => handleSquareClick(8)}/>
     </div>
   </>
 );
